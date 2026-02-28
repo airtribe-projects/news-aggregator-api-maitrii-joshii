f76ac9c76fc3df7a97327b5f59966c05b01bbb54
mj <maitrijoshi1710@gmail.com>
2026-02-28 17:07:06 +0530

    Added Validation

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ const port = 3000;
 const authRouter = require('./routers/authRouter');
 const userPreferencesRouter = require('./routers/userPreferencesRouter');
 const newsRouter = require('./routers/newsRouter');
+const errorHandler = require('./middlewares/errorHandler');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRouter);
 app.use("/preferences", userPreferencesRouter);
 app.use("/news", newsRouter);
+app.use(errorHandler);
 
 app.listen(port, (err) => {
     if (err) {
--- src/routers/userPreferencesRouter.js
+++ src/routers/userPreferencesRouter.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { validateJWT } = require('../middlewares/authMiddleware');
 const { createUserPreferences, retrieveUserPreferences, updateUserPreferences } = require('../controllers/userPreferencesController');
+const { userPreferencesSchema } = require('../schemas/userPreferencesSchema');
+const { validateRequest } = require('../middlewares/validateRequest');
 
 router.use(validateJWT);
 
-router.post("", createUserPreferences);
+router.post("", validateRequest(userPreferencesSchema), createUserPreferences);
 router.get("", retrieveUserPreferences);
-router.put("/:id", updateUserPreferences);
+router.put("/:id", validateRequest(userPreferencesSchema), updateUserPreferences);
 
 
 module.exports = router;
